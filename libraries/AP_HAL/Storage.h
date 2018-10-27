a73a0eda6822fe44f32f2ac3307ef6b598d6357e
testjig <siddharthbharatpurohit@gmail.com>
2018-10-27 20:38:06 +0800

    HAL: add support for OTP program

--- libraries/AP_HAL/Storage.h
+++ libraries/AP_HAL/Storage.h
@@ -10,5 +10,9 @@ public:
     virtual void read_block(void *dst, uint16_t src, size_t n) = 0;
     virtual void write_block(uint16_t dst, const void* src, size_t n) = 0;
     virtual void _timer_tick(void) {};
+
+    virtual bool write_otp(void *buf, uint8_t region, uint8_t size) { return false; }
+    virtual bool lock_otp(uint8_t region) { return false; }
+    virtual uint8_t read_otp(void *buf, uint8_t region, uint8_t size) { return false; }
     virtual bool healthy(void) { return true; }
 };
