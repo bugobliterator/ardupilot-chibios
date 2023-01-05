eed66ce28c8f4126576fcc50209fae827644e018
bugobliterator <siddharthbharatpurohit@gmail.com>
2023-01-05 11:21:20 +1100

    AP_EFI: replace libuavcan with libcanard based driver

--- libraries/AP_EFI/AP_EFI_DroneCAN.h
+++ libraries/AP_EFI/AP_EFI_DroneCAN.h
@@ -4,14 +4,11 @@
 #include "AP_EFI_Backend.h"
 
 #ifndef HAL_EFI_DRONECAN_ENABLED
-#define HAL_EFI_DRONECAN_ENABLED HAL_MAX_CAN_PROTOCOL_DRIVERS && BOARD_FLASH_SIZE > 1024 && HAL_CANMANAGER_ENABLED
+#define HAL_EFI_DRONECAN_ENABLED HAL_MAX_CAN_PROTOCOL_DRIVERS && BOARD_FLASH_SIZE > 1024 && HAL_ENABLE_LIBUAVCAN_DRIVERS
 #endif
 
 #if HAL_EFI_DRONECAN_ENABLED
 #include <AP_UAVCAN/AP_UAVCAN.h>
-#include <uavcan/equipment/ice/reciprocating/Status.hpp>
-
-class EFIStatusCb;
 
 class AP_EFI_DroneCAN : public AP_EFI_Backend {
 public:
@@ -20,10 +17,10 @@ public:
     void update() override;
 
     static void subscribe_msgs(AP_UAVCAN* ap_uavcan);
-    static void trampoline_status(AP_UAVCAN* ap_uavcan, uint8_t node_id, const EFIStatusCb &cb);
+    static void trampoline_status(AP_UAVCAN *ap_uavcan, const CanardRxTransfer& transfer, const uavcan_equipment_ice_reciprocating_Status &msg);
 
 private:
-    void handle_status(const uavcan::equipment::ice::reciprocating::Status &pkt);
+    void handle_status(const uavcan_equipment_ice_reciprocating_Status &pkt);
 
     // singleton for trampoline
     static AP_EFI_DroneCAN *driver;
